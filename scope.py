a72ec939ba8cc39e3fa0fb148b9613a75d04b460
David Michel <anonymous@nothing.com>
2017-06-05 11:45:24 +0100

    remove redundant profiler module - added code to scope.py

--- scope.py
+++ scope.py
@@ -1,7 +1,6 @@
 import visa
 import time
 
-
 class Scope(object):
     """ Scope instrument """
 
@@ -134,3 +133,25 @@ class Scope(object):
 
     def close(self):
         self.c.close()
+
+
+
+import sys
+import os
+
+if __name__ == '__main__':
+
+    try:
+        scope = Scope('TCPIP::192.168.41.52::5025::SOCKET', 'chan1')
+        scope.setPulse(2e-9, 1.2e-8)
+        while True:
+            scope.fetch()
+            scope.savePulse()
+
+    except KeyboardInterrupt:
+        print 'Interrupted'
+        try:
+            scope.close()
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
