befd8a4a1eefdac86886723781ee3544be0d1842
Chris Gregory <61797427+chris-d-gregory@users.noreply.github.com>
2018-09-17 09:38:39 +0100

    TZ20160417_014: improved simulation mode so that it converges on the target and has background subtraction

--- header.py
+++ header.py
@@ -1,8 +1,8 @@
 #########################################################################################
 #Any constants that are needed
 SCOPE_WAIT_TIME = 2.1 # Seconds to wait for a caget from the scope to return
-SIMULATION = True
-DIAG_FILE_LOCATION = "./diag_files/180830/"
+SIMULATION = False
+DIAG_FILE_LOCATION = "./diag_files/180917/"
 AWG_PREFIX = "AWG"
 DEFAULT_SCOPE_PV="CO-SCOPE-2:CH2:ReadWaveform"
 PAUSE_BETWEEN_AWG_WRITE = 0.3
