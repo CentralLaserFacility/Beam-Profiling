3ffd73ac4469b43c7a92df6d63953c5018ab3516
Chris Gregory <61797427+chris-d-gregory@users.noreply.github.com>
2019-08-01 15:42:19 +0100

    User-defined filter to be applied to AWG traces before sending to device

---
+++ user_filter.py
@@ -0,0 +1,21 @@
+#####################################################################################
+#  User defined filter that is applied to the AWG trace before appling.             #
+#                                                                                   #
+#  The function must be named user_filter                                           #
+#                                                                                   #
+#  The function takes an array of any length and returns the filtered array, which  #
+#  must be of the same length                                                       #
+#                                                                                   #
+# ################################################################################### 
+
+import numpy as np 
+from scipy.ndimage.filters import gaussian_filter1d
+from scipy.signal import medfilt
+
+def gaussian_filter(data):
+    return gaussian_filter1d(data, sigma=1, order=0) 
+
+def median_filter(data):
+    return medfilt(data, 11)
+
+user_filter = gaussian_filter
